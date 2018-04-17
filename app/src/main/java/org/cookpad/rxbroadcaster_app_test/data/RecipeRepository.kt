99df0e0265d8ac0b996032925d6364c912e944c1
Pablo Pernias <pablo@pernias.com>
2018-04-17 11:28:06 +0200

    add third recipe to repository + make longer descriptions

--- app/src/main/java/org/cookpad/rxbroadcaster_app_test/data/RecipeRepository.kt
+++ app/src/main/java/org/cookpad/rxbroadcaster_app_test/data/RecipeRepository.kt
@@ -5,8 +5,15 @@ import io.reactivex.Single
 import org.cookpad.rxbroadcaster_app_test.data.models.Recipe
 
 private val recipes = mutableListOf(
-        Recipe("1", "Chicken", "A very good chicken", false, false),
-        Recipe("2", "Salad", "A very good salad", false, true)
+        Recipe("1", "Chicken",
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ultrices felis et dolor maximus lobortis. Curabitur et semper enim, vitae molestie purus. Etiam sed euismod massa. Donec a placerat dui, pretium posuere felis. Phasellus volutpat libero mattis neque ultrices, a molestie lectus tempor. Duis interdum nunc quis nisi volutpat, eget egestas mauris congue. Nunc pulvinar id orci at scelerisque.",
+                false, false),
+        Recipe("2", "Salad",
+                "Praesent mattis ligula lectus. Morbi tincidunt lorem vitae ipsum sagittis convallis. Nam at sapien at nibh posuere mollis ut et tortor. Cras vel tellus in massa malesuada accumsan vitae sed ex. Maecenas lobortis, sapien a eleifend fringilla, turpis mi dignissim mauris, eget aliquam ex nulla vel magna. Pellentesque in ipsum sed leo blandit egestas. Nullam eu arcu ut eros blandit interdum. Aliquam erat volutpat. In blandit fringilla dolor eu tempus.",
+                false, true),
+        Recipe("3", "Pasta",
+                "Curabitur non eleifend tellus, at luctus sapien. Phasellus ut sodales dolor. Praesent consectetur elit quis odio egestas consectetur. Morbi venenatis ac justo a ullamcorper. Phasellus turpis lacus, scelerisque ut lectus eget, bibendum sagittis felis. Suspendisse potenti. Sed arcu odio, sollicitudin interdum sodales eu, eleifend non velit. Vivamus placerat dolor sit amet nisl aliquet tempus. Pellentesque sed pulvinar ante. Morbi risus odio, viverra non arcu sit amet, eleifend commodo massa.",
+                false, false)
 )
 
 class RecipeRepository {
