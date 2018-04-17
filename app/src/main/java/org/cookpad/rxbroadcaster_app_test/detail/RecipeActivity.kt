299b02bc47c0c4fe31cdc2decccdcc36a74aa92d
Pablo Pernias <pablo@pernias.com>
2018-04-17 11:30:29 +0200

    add actions to recipe activity and connect it with the list to manually update states

--- app/src/main/java/org/cookpad/rxbroadcaster_app_test/detail/RecipeActivity.kt
+++ app/src/main/java/org/cookpad/rxbroadcaster_app_test/detail/RecipeActivity.kt
@@ -3,24 +3,48 @@ package org.cookpad.rxbroadcaster_app_test.detail
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
+import io.reactivex.subjects.PublishSubject
 import kotlinx.android.synthetic.main.activity_recipe.*
 import org.cookpad.rxbroadcaster_app_test.R
 import org.cookpad.rxbroadcaster_app_test.data.models.Recipe
+import org.cookpad.rxbroadcaster_app_test.home.recipes.RecipesFragment
 
 class RecipeActivity : AppCompatActivity(), RecipePresenter.View {
     override val recipeId by lazy { intent.extras.getString(keyRecipeId) }
+
+    val onRecipeLikedSubject = PublishSubject.create<Unit>()
+    override val onRecipeLiked = onRecipeLikedSubject.hide()
+
+    val onRecipeBookmarkedSubject = PublishSubject.create<Unit>()
+    override val onRecipeBookmarked = onRecipeBookmarkedSubject.hide()
+
+    val onRecipeUpdatedFromListSubject = PublishSubject.create<Recipe>()
+    override val onRecipeUpdatedFromList = onRecipeUpdatedFromListSubject.hide()
+
+    override val onRecipeUpdatedFromDetailSubject = PublishSubject.create<Recipe>()
+    val onRecipeUpdatedFromDetail = onRecipeUpdatedFromDetailSubject.hide()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recipe)
 
         lifecycle.addObserver(RecipePresenter(this))
+
+        (fragmentRecipes as? RecipesFragment)?.apply {
+            this.onRecipeUpdatedSubject = onRecipeUpdatedFromListSubject
+            this.onRecipeUpdated = onRecipeUpdatedFromDetail
+        }
+
+        ivLikeButton.setOnClickListener { onRecipeLikedSubject.onNext(Unit) }
+        ivBookmarkButton.setOnClickListener { onRecipeBookmarkedSubject.onNext(Unit) }
     }
 
     override fun showRecipe(recipe: Recipe) {
         recipe.apply {
             tvTitle.text = name
+            tvDescription.text = description
         }
     }
 
@@ -33,6 +57,26 @@ class RecipeActivity : AppCompatActivity(), RecipePresenter.View {
             }
 
             context.startActivity(intent)
+        }
+    }
+
+    override fun setLiked(liked: Boolean) {
+        ivLikeButton.apply {
+            val (drawable, colorFilter) = if (liked) {
+                R.drawable.ic_liked to ContextCompat.getColor(context, R.color.likedColor)
+            } else {
+                R.drawable.ic_like to ContextCompat.getColor(context, R.color.textColor)
+            }
+
+            setImageResource(drawable)
+            setColorFilter(colorFilter)
+        }
+    }
+
+    override fun setBookmarked(bookmarked: Boolean) {
+        ivBookmarkButton.apply {
+            val drawableBookmark = if (bookmarked) R.drawable.ic_bookmarked else R.drawable.ic_bookmark
+            setImageResource(drawableBookmark)
         }
     }
 }
--- app/src/main/java/org/cookpad/rxbroadcaster_app_test/detail/RecipePresenter.kt
+++ app/src/main/java/org/cookpad/rxbroadcaster_app_test/detail/RecipePresenter.kt
@@ -3,7 +3,9 @@ package org.cookpad.rxbroadcaster_app_test.detail
 import android.arch.lifecycle.Lifecycle
 import android.arch.lifecycle.LifecycleObserver
 import android.arch.lifecycle.OnLifecycleEvent
+import io.reactivex.Observable
 import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.subjects.PublishSubject
 import org.cookpad.rxbroadcaster_app_test.data.RecipeRepository
 import org.cookpad.rxbroadcaster_app_test.data.models.Recipe
 import org.cookpad.rxbroadcaster_app_test.utils.extensions.addTo
@@ -16,9 +18,44 @@ class RecipePresenter(private val view: View,
     fun onCreate() {
         view.apply {
             repository.get(recipeId)
-                    .subscribe { recipe -> showRecipe(recipe) }
+                    .subscribe { recipe ->
+                        showRecipe(recipe)
+                        setBookmarked(recipe.bookmarked)
+                        setLiked(recipe.liked)
+                    }
                     .addTo(disposables)
 
+            onRecipeLiked
+                    .flatMapSingle { repository.get(recipeId) }
+                    .flatMapCompletable { recipe ->
+                        val updatedRecipe = recipe.copy(liked = !recipe.liked)
+                        repository.updateRecipe(updatedRecipe).doOnComplete {
+                            setLiked(updatedRecipe.liked)
+                            onRecipeUpdatedFromDetailSubject.onNext(updatedRecipe)
+                        }
+                    }
+                    .subscribe()
+                    .addTo(disposables)
+
+            onRecipeBookmarked
+                    .flatMapSingle { repository.get(recipeId) }
+                    .flatMapCompletable { recipe ->
+                        val updatedRecipe = recipe.copy(bookmarked = !recipe.bookmarked)
+                        repository.updateRecipe(updatedRecipe).doOnComplete {
+                            setBookmarked(updatedRecipe.bookmarked)
+                            onRecipeUpdatedFromDetailSubject.onNext(updatedRecipe)
+                        }
+                    }
+                    .subscribe()
+                    .addTo(disposables)
+
+            onRecipeUpdatedFromList
+                    .flatMapSingle { repository.get(recipeId) }
+                    .subscribe { recipe ->
+                        setBookmarked(recipe.bookmarked)
+                        setLiked(recipe.liked)
+                    }
+                    .addTo(disposables)
         }
     }
 
@@ -28,7 +65,14 @@ class RecipePresenter(private val view: View,
     }
 
     interface View {
+        val onRecipeLiked: Observable<Unit>
+        val onRecipeBookmarked: Observable<Unit>
+        val onRecipeUpdatedFromList: Observable<Recipe>
+        val onRecipeUpdatedFromDetailSubject: PublishSubject<Recipe>
+
         val recipeId: String
         fun showRecipe(recipe: Recipe)
+        fun setLiked(liked: Boolean)
+        fun setBookmarked(bookmarked: Boolean)
     }
 }
